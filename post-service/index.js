f2aca5c353c63b931d824fa93f472982e01ff5c9
Mariana1010P <portela.mariana.6654@eam.edu.co>
2025-05-15 12:06:21 -0500

    feat(frontend): agregar lógica de retry y circuito de protección en creación de posts

--- post-service/index.js
+++ post-service/index.js
@@ -6,11 +6,16 @@ app.use(express.json());
 let posts = [];
 
 app.post("/posts", (req, res) => {
+    if (Math.random() < 0.5) {
+        return res.status(500).json({ error: "Error simulado del servicio" });
+    }
     const { title, content } = req.body;
     const newPost = { id: Date.now(), title, content };
     posts.push(newPost);
     res.status(201).json(newPost);
 });
+
+
 
 app.get("/posts", (req, res) => {
     res.json(posts);
